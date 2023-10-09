261f79d7e070ec3bac7cc82ce69d1a194fcf6f15
Arctyo <arctiongamz@gmail.com>
2023-10-09 20:03:59 +0200

    Updated All

--- VJ1/V1-1.c
+++ VJ1/V1-1.c
@@ -7,9 +7,37 @@
 #include <errno.h>
 #include <math.h>
 
+
+
+typedef struct Student
+{
+	char ime[50];
+	char prezime[50];
+	int bod;
+
+} Studi;
+
+
+
 int main()
 {
+	char temp[1024] = "";
+	int broj = 0;
+	FILE* fp = NULL;
 
+	fp = fopen("StudentsAmogus.txt", "r");
+
+	if (fp == NULL)
+	{
+		printf("Nema datoteke!");
+		return 1;
+	}
+
+	while (!feof(fp))
+	{
+		if (fgets(temp, 1024, fp))
+			broj++;
+	}
 
 	return 0;
 }

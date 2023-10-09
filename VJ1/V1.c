ab6153987ebbdde8d5ec77cdb829323f6bb9dee4
Arctyo <arctiongamz@gmail.com>
2023-10-09 19:15:20 +0200

    Changed V1 file

--- VJ1/V1.c
+++ VJ1/V1.c
@@ -1,5 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <math.h>
 
 int main()
 {

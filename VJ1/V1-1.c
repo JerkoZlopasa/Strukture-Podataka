2f6cb99fcd569c7d7c9f9457a7f731a49e01d6e4
Arctyo <arctiongamz@gmail.com>
2023-10-16 19:11:20 +0200

    Divided code into functions

--- VJ1/V1-1.c
+++ VJ1/V1-1.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define MAXLIN 1024
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -19,13 +20,9 @@ typedef struct Student
 
 
 
-int main()
+int BrojiRetke(int a, char* buffer)
 {
-	//Deklariranje Varijabli, temp je tun za brojanje redaka
-	char temp[1024] = "";
-	int broj = 0, i = 0, najbod = 0;
 	FILE* fp = NULL;
-	Studi* studarr;
 
 
 	//Otvaranje datoteke i provjera otvaranja
@@ -43,28 +40,55 @@ int main()
 
 	while (!feof(fp))
 	{
-		if (fgets(temp, 1024, fp))
-			broj++;
+		if (fgets(buffer, 1024, fp))
+			a++;
 	}
 
+	fclose(fp);
 
+	return a;
+}
+
+
+
+Studi* Alociraj(Studi* pointstud, int broj)
+{
 	//Alokacija prostora i provjera pointera
-	studarr = (Studi*)malloc(broj * sizeof(Studi));
+	pointstud = (Studi*)malloc(broj * sizeof(Studi));
 
-	if (studarr == NULL)
+	if (pointstud == NULL)
 	{
 		printf("Neuspjela alokacija!");
 		return 1;
 	}
 
+	return pointstud;
+}
 
-	//Resetiranje pointera na pocetak filea, preskakanje header retka i append u listu s provjerun maks bodova pa zatvaranje
-	rewind(fp);
+
+
+int Pridaj(Studi* studarr, int najbod)
+{
+	int i = 0;
+	FILE* fp = NULL;
+
+
+	//Otvaranje datoteke i provjera otvaranja
+	fp = fopen("StudentsAmogus.txt", "r");
+
+	if (fp == NULL)
+	{
+		printf("Nema datoteke!");
+		return 1;
+	}
+
+
+	//Preskakanje header retka i append u listu s provjerun maks bodova pa zatvaranje
 	while (fgetc(fp) != '\n');
 
 	while (!feof(fp))
 	{
-		fscanf(fp, "%s %s %d", (studarr+i)->ime, (studarr+i)->prezime, &studarr[i].bod);
+		fscanf(fp, "%s %s %d", (studarr + i)->ime, (studarr + i)->prezime, &studarr[i].bod);
 
 		(studarr[i].bod > najbod) ? najbod = studarr[i].bod : 1;
 		i++;
@@ -72,11 +96,45 @@ int main()
 
 	fclose(fp);
 
+	return najbod;
+}
 
+
+
+void Ispis(Studi* studarr, int br, int najbod)
+{
+	int i = 0;
 	//Ispis svega sto je potrebno
 	printf("IME\tPREZIME\tBODOVI\tREL.BOD");
-	for (i = 0; i < broj; i++)
-		printf("\n%s\t%s\t%d\t%f", studarr[i].ime, studarr[i].prezime, studarr[i].bod, (float)studarr[i].bod/(float)najbod*100);
+	for (i = 0; i < br; i++)
+		printf("\n%s\t%s\t%d\t%f", studarr[i].ime, studarr[i].prezime, studarr[i].bod, (float)studarr[i].bod / (float)najbod * 100);
 
+	printf("\n");
+
+}
+
+
+
+int main()
+{
+	//Deklariranje Varijabli, temp je tun za brojanje redaka
+	char temp[MAXLIN] = "";
+	int broj = 0, najbod = 0;
+	Studi* studarr = NULL;
+
+	//Brojanje studenata u fileu, vraca broj
+	broj = BrojiRetke(broj, temp);
+	
+	//Alociranje prostora za listu studenata, vraca tu listu
+	studarr = Alociraj(studarr, broj);
+	
+	//Funkcija citanja i dodavanja u listu studenata, dodatno racuna najbolji broj bodova
+	najbod = Pridaj(studarr, najbod);
+
+	//Funkcija ispisa
+	Ispis(studarr, broj, najbod);
+
+
+	
 	return 0;
 }
